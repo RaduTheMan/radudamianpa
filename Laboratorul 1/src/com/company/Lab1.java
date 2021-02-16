d54c5bd844294e78d1e6491f20481053942319cd
RaduTheMan <radu.damian07@gmail.com>
2021-02-16 12:52:59 +0200

    mica rafinare calcule int n

--- Laboratorul 1/src/com/company/Lab1.java
+++ Laboratorul 1/src/com/company/Lab1.java
@@ -9,16 +9,21 @@ public class Lab1 {
         Lab1 obj = new Lab1();
         obj.compulsory();
     }
+    private void modifyN()
+    {
+        n *= 3;
+        n += 0b10101;
+        n += 0xFF;
+        n *= 6;
+    }
     public void compulsory()
     {
         System.out.println("Hello world!");
         languages = new String[] {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
         n = (int) (Math.random() * 1_000_000);
-        n *= 3;
-        n += 0b10101;
-        n += 0xFF;
-        n *= 6;
-        
+        System.out.println(n);
+        this.modifyN();
+        System.out.println(n);
     }
 
 }
