ebea2be6e17452f334a8c53ce311a67c25dc6dfb
RaduTheMan <radu.damian07@gmail.com>
2021-02-16 12:48:10 +0200

    integer n calculations

--- Laboratorul 1/src/com/company/Lab1.java
+++ Laboratorul 1/src/com/company/Lab1.java
@@ -3,6 +3,7 @@ package com.company;
 public class Lab1 {
 //Damian Radu
     private String languages[];
+    private int n;
     public static void main(String[] args) {
 
         Lab1 obj = new Lab1();
@@ -12,6 +13,12 @@ public class Lab1 {
     {
         System.out.println("Hello world!");
         languages = new String[] {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        n = (int) (Math.random() * 1_000_000);
+        n *= 3;
+        n += 0b10101;
+        n += 0xFF;
+        n *= 6;
+        
     }
 
 }
