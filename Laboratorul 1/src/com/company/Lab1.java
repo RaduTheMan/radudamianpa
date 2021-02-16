c581b891dfd5000e9117d737134077915abb412b
RaduTheMan <radu.damian07@gmail.com>
2021-02-16 12:41:44 +0200

    string array with languages

--- Laboratorul 1/src/com/company/Lab1.java
+++ Laboratorul 1/src/com/company/Lab1.java
@@ -2,17 +2,16 @@ package com.company;
 
 public class Lab1 {
 //Damian Radu
-    
+    private String languages[];
     public static void main(String[] args) {
 
         Lab1 obj = new Lab1();
         obj.compulsory();
-
-
     }
     public void compulsory()
     {
         System.out.println("Hello world!");
+        languages = new String[] {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
     }
 
 }
