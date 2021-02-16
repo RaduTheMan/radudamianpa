d478c73aa8fcc6441a50067854e6ba16f7051502
RaduTheMan <radu.damian07@gmail.com>
2021-02-16 12:33:50 +0200

    Start proiect

---
+++ Laboratorul 1/src/com/company/Lab1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Lab1 {
+//Damian Radu
+    
+    public static void main(String[] args) {
+
+        Lab1 obj = new Lab1();
+        obj.compulsory();
+
+
+    }
+    public void compulsory()
+    {
+        System.out.println("Hello world!");
+    }
+
+}
