522b2b4346618ec9591fb430cbcf1069dafa735c
RaduTheMan <radu.damian07@gmail.com>
2021-02-28 17:35:55 +0200

    creating instance of a problem

--- Laboratorul2/src/main/java/com/mycompany/laboratorul2/Lab2.java
+++ Laboratorul2/src/main/java/com/mycompany/laboratorul2/Lab2.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package com.mycompany.laboratorul2;
-
 /**
  *
  * @author Radu
@@ -12,7 +11,29 @@ package com.mycompany.laboratorul2;
 public class Lab2 {
     public static void main (String[] args)
     {
-        System.out.println("Hello world");
+        Problem pb = new Problem(5,5);
+        int supply[] = new int[] {10,35,25};
+        int demand[] = new int[] {20,25,25};
+        int cost[][] = new int[][] {{2,3,1}, {5,4,8}, {5,6,8}};
+        Source source1 = new Source("S1",SourceType.FACTORY);
+        Source source2 = new Source("S2",SourceType.WAREHOUSE);
+        Source source3 = new Source("S3",SourceType.WAREHOUSE);
+        Destination destination1 = new Destination("D1");
+        Destination destination2 = new Destination("D2");
+        Destination destination3 = new Destination("D3");
+        pb.addSource(source1);
+        pb.addSource(source2);
+        pb.addSource(source3);
+        pb.addDestination(destination1);
+        pb.addDestination(destination2);
+        pb.addDestination(destination3);
+        pb.setDemand(demand);
+        pb.setSupply(supply);
+        pb.setCost(cost);
+        
+        
+        
+        
     }
     
 }
--- Laboratorul2/src/main/java/com/mycompany/laboratorul2/Problem.java
+++ Laboratorul2/src/main/java/com/mycompany/laboratorul2/Problem.java
@@ -18,4 +18,80 @@ public class Problem {
     private int demand[];
     private int cost[][];
     
+    private int nrSources;
+    private int nrDestinations;
+
+    public Problem(int nrSourcesMax, int nrDestinationsMax) {
+        sources = new Source[nrSourcesMax];
+        destinations = new Destination[nrDestinationsMax];
+        nrSources = 0;
+        nrDestinations = 0;
+    }
+    
+    public void addSource(Source source)
+    {
+        if(nrSources+1 <= sources.length)
+        {
+            sources[nrSources++] = source;
+        }
+        else
+            System.out.println("The maximum number of sources allowed was reached!");
+    }
+    
+    public void addDestination(Destination destination)
+    {
+        if(nrDestinations+1 <= destinations.length)
+        {
+            destinations[nrDestinations++] = destination;
+        }
+        else
+            System.out.println("The maximum number of destinations allowed was reached!");
+    }
+
+    public void setSupply(int[] supply) {
+        if(supply.length == nrSources)
+          this.supply = supply;
+        else
+          System.out.println("Number of supplies is not equal with the number of sources of the problem");
+    }
+
+    public void setDemand(int[] demand) {
+       if(demand.length == nrDestinations) 
+         this.demand = demand;
+       else
+         System.out.println("Number of demands is not equal with the number of destinations of the problem");
+    }
+
+    public void setCost(int[][] cost) {
+        if(cost.length == nrSources)
+        {
+            if(cost.length == 0)
+            {
+                if( nrDestinations == 0)
+                     this.cost = cost;
+            }
+            else
+            {
+            if(cost[0].length == nrDestinations)
+               this.cost = cost;
+            else
+               System.out.println("The cost matrix doesn't have the coresponding number of sources/destinations of the problem");
+            
+            }
+        }
+        else
+            System.out.println("The cost matrix doesn't have the coresponding number of sources/destinations of the problem");
+        
+    }
+    
+    
+
+   
+    
+    
+    
+    
+    
+    
+    
 }
