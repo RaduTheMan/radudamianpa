2926dfef0b8eb892f4f720668eb7b1824136c510
RaduTheMan <radu.damian07@gmail.com>
2021-03-02 11:55:34 +0200

    creating lab 3 project

---
+++ Laboratorul3/src/main/java/com/mycompany/laboratorul3/Lab3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.laboratorul3;
+
+/**
+ *
+ * @author Radu
+ */
+public class Lab3 {
+    
+    public static void main(String args[])
+    {
+        System.out.println("Hello world!!");
+    }
+    
+}
