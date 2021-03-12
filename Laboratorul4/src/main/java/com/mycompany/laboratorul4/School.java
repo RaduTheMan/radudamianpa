14915718ae2bf3c031c855ab5cec9bce26e47098
RaduTheMan <radu.damian07@gmail.com>
2021-03-12 22:42:08 +0200

    creating lab 4 project

---
+++ Laboratorul4/src/main/java/com/mycompany/laboratorul4/School.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.laboratorul4;
+
+/**
+ *
+ * @author Radu
+ */
+public class School {
+    private String name;
+
+    public School(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
