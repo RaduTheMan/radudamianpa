881e9f588029b7ddfe2b6ddec7478a65f734081f
RaduTheMan <radu.damian07@gmail.com>
2021-03-16 11:44:43 +0200

    creating lab 5 project

---
+++ Laboratorul5/src/main/java/com/mycompany/laboratorul5/Lab5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.laboratorul5;
+
+/**
+ *
+ * @author Radu
+ */
+public class Lab5 {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("hello from lab 5!");
+    }
+    
+}
