41dcbba770d851bf299df97611926698fba5dbc9
RaduTheMan <radu.damian07@gmail.com>
2021-04-16 22:37:27 +0300

    updated sql script

--- Laboratorul8/creare_filme.sql
+++ Laboratorul8/creare_filme.sql
@@ -7,7 +7,7 @@ DROP TABLE movie_genre_assoc
 
 
 CREATE TABLE movies(
-  id NUMBER(4) NOT NULL PRIMARY KEY,
+  id_movie NUMBER(4) NOT NULL PRIMARY KEY,
   title VARCHAR2(15),
   release_date DATE,
   duration INTERVAL DAY TO SECOND,
@@ -17,14 +17,16 @@ CREATE TABLE movies(
 
 
 CREATE TABLE genres(
-  id NUMBER(4) NOT NULL PRIMARY KEY,
+  id_genre NUMBER(4) NOT NULL PRIMARY KEY,
   name VARCHAR2(15)
   )
 /
 
 CREATE TABLE movie_genre_assoc(
   id_movie NUMBER(4) NOT NULL,
-  id_genre NUMBER(4) NOT NULL
+  id_genre NUMBER(4) NOT NULL,
+  CONSTRAINT fk_id_movie FOREIGN KEY (id_movie) REFERENCES movies(id_movie),
+  CONSTRAINT fk_id_genre FOREIGN KEY (id_genre) REFERENCES genres(id_genre)
   )
 /
 
@@ -32,10 +34,10 @@ INSERT INTO movies VALUES (1, 'Airplane', TO_DATE('02/07/1980', 'dd/mm/yyyy'), I
 INSERT INTO movies VALUES (2, 'The Father', TO_DATE('26/02/2021', 'dd/mm/yyyy'), INTERVAL '0 1:37:00' DAY TO SECOND, 8);
 INSERT INTO movies VALUES (3, 'Pulp Fiction', TO_DATE('14/10/1994', 'dd/mm/yyyy'), INTERVAL '0 2:34:00' DAY TO SECOND, 9);
 
-INSERT INTO genres VALUES ();
-INSERT INTO genres VALUES ();
-INSERT INTO genres VALUES ();
+INSERT INTO genres VALUES (1, 'Drama');
+INSERT INTO genres VALUES (2, 'Comedie');
+INSERT INTO genres VALUES (3, 'Actiune');
 
-INSERT INTO movie_genre_assoc VALUES ();
-INSERT INTO movie_genre_assoc VALUES ();
-INSERT INTO movie_genre_assoc VALUES ();
+INSERT INTO movie_genre_assoc VALUES (1,2);
+INSERT INTO movie_genre_assoc VALUES (2,1);
+INSERT INTO movie_genre_assoc VALUES (3,3);
