89a58ca4cbb44304cb2c948ebe464b4053518fc0
RaduTheMan <radu.damian07@gmail.com>
2021-04-20 00:36:51 +0300

    creating necessary tables for storing actors and directors

--- Laboratorul8/src/main/java/com/mycompany/laboratorul8/Main.java
+++ Laboratorul8/src/main/java/com/mycompany/laboratorul8/Main.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package com.mycompany.laboratorul8;
-import java.sql.*;
 import java.time.Duration;
 /**
  *
--- Laboratorul8/src/main/java/com/mycompany/laboratorul8/MovieDaoImpl.java
+++ Laboratorul8/src/main/java/com/mycompany/laboratorul8/MovieDaoImpl.java
@@ -10,8 +10,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
