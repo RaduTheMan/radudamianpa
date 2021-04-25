fff3567708f193a41b3d654395a60ba3a04202ce
RaduTheMan <radu.damian07@gmail.com>
2021-04-25 20:59:21 +0300

    solving some bugs and testing a simple persistance: adding an actor into the database

--- Laboratorul9/src/main/java/com/mycompany/laboratorul9/main/Main.java
+++ Laboratorul9/src/main/java/com/mycompany/laboratorul9/main/Main.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.laboratorul9.main;
 
+import com.mycompany.laboratorul9.entityclasses.Actor;
 import com.mycompany.laboratorul9.singleton.jpa.EntityManagerSingleton;
 
 
@@ -16,11 +17,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-      EntityManagerSingleton ems = EntityManagerSingleton.getInstance();
-      ems.createEntityManager();
-      System.out.println(ems.getEntityManager().toString());
+      testCreateActor();
       
         
+    }
+    
+    private static void testCreateActor()
+    {
+      EntityManagerSingleton ems = EntityManagerSingleton.getInstance();
+      ems.createEntityManager();
+      ems.getEntityManager().getTransaction().begin();
+      Actor actor = new Actor();
+      actor.setIdActor((short)20);
+      actor.setFirstName("Radu");
+      actor.setLastName("Damian");
+      ems.getEntityManager().persist(actor);
+      ems.getEntityManager().getTransaction().commit();
+      ems.closeEntityManager();
+      ems.closeEntityManagerFactory();
+      System.out.println(actor);
     }
     
 }
