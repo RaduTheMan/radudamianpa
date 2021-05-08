07e2c07fbe43f3d36fe26e683ff6a01b9204a4d3
RaduTheMan <radu.damian07@gmail.com>
2021-05-09 00:50:57 +0300

    adding a thread pool

--- Laboratorul10/Server/src/main/java/com/mycompany/server/SimpleServer.java
+++ Laboratorul10/Server/src/main/java/com/mycompany/server/SimpleServer.java
@@ -8,6 +8,8 @@ package com.mycompany.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *
@@ -16,6 +18,7 @@ import java.net.Socket;
 public class SimpleServer {
     
     public static final int PORT = 8100;
+    public static final int MAX_T = 4;
     
     public SimpleServer() throws IOException {
         ServerSocket serverSocket = null;
@@ -27,7 +30,10 @@ public class SimpleServer {
                 System.out.println("Waiting for a client...");
                 Socket socket = serverSocket.accept();
                 //execute client request in a thread from the thread pool(TO DO)
-                new ClientThread(socket).start();
+//                new ClientThread(socket).start();
+                ExecutorService pool = Executors.newFixedThreadPool(MAX_T);
+                
+                pool.execute(new ClientThread(socket));
             }
             
         } catch (IOException ex) {
